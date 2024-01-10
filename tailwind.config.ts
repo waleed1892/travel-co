eef35edc613b3d059a2be8fc91144723a2cd146b
Waleed Zahid <waleed1892@gmail.com>
2024-01-10 20:48:51 +0500

    task: added base styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,60 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1.5rem",
+        lg: "5rem",
+        "3xl": "0rem",
+      },
+    },
     extend: {
+      colors: {
+        green: {
+          50: "#30AF5B",
+          100: "#292C27",
+        },
+        gray: {
+          100: "#EEEEEE",
+          200: "#A2A2A2",
+          300: "#7B7B7B",
+          500: "#585858",
+          900: "#141414",
+        },
+        orange: {
+          50: "#FF814C",
+        },
+        blue: {
+          50: "#021639",
+        },
+        yellow: {
+          50: "#FEC601",
+        },
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "bg-img-1": "url('/img-1.png')",
+        "bg-img-2": "url('/img-2.png')",
+        "feature-bg": "url('/feature-bg.png')",
+        pattern: "url('/pattern.png')",
+        "pattern-2": "url('/pattern-bg.png')",
+      },
+      screens: {
+        xs: "400px",
+        "3xl": "1680px",
+        "4xl": "2200px",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
