9e941e8929f349d85e80867c623ae9d392b3fe04
Waleed Zahid <waleed1892@gmail.com>
2024-01-11 18:07:42 +0500

    added base navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,25 @@
+import { navLinks } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+
 export const Navbar = () => {
-  return <nav className='px'></nav>;
+  return (
+    <nav className='container flex justify-between z-30 py-5'>
+      <Link href={"/"}>
+        <Image width={150} height={80} src={"/logo.svg"} alt='TravelCo' />
+      </Link>
+      <ul className='hidden lg:flex h-full gap-12'>
+        {navLinks.map((link) => (
+          <li key={link.name} className='flex items-center h-full'>
+            <Link
+              className='text-base text-gray-500 flex items-center justify-center pb-1.5 transition-all hover:font-bold'
+              href={link.href}
+            >
+              {link.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
---
+++ constants/index.ts
@@ -0,0 +1,14 @@
+export const navLinks = [
+  {
+    name: "Home",
+    href: "/",
+  },
+  {
+    name: "About",
+    href: "/about",
+  },
+  {
+    name: "Contact",
+    href: "/contact",
+  },
+];
