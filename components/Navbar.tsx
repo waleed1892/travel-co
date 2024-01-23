99622db1d1d3400f21fa044cee756f296bec180a
Waleed Zahid <waleed1892@gmail.com>
2024-01-23 20:03:22 +0500

    finished navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,11 @@
 import { navLinks } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
+import { Button } from './ui/button'
 
 export const Navbar = () => {
   return (
-    <nav className="container z-30 flex justify-between py-5">
+    <nav className="container z-30 flex items-center justify-between py-5">
       <Link href={'/'}>
         <Image width={150} height={80} src={'/logo.svg'} alt="TravelCo" />
       </Link>
@@ -20,6 +21,31 @@ export const Navbar = () => {
           </li>
         ))}
       </ul>
+      <div className="hidden lg:flex lg:items-center lg:justify-center">
+        <Button
+          className="gap-3 rounded-full bg-green-900 text-white"
+          size={'lg'}
+        >
+          <Image
+            src={'/assets/images/user.svg'}
+            height={24}
+            width={24}
+            alt="user"
+          />
+          Login
+        </Button>
+      </div>
+      <Button
+        variant={'ghost'}
+        className="inline-block hover:bg-transparent lg:hidden"
+      >
+        <Image
+          src={'/assets/images/menu.svg'}
+          height={32}
+          width={32}
+          alt="menu"
+        />
+      </Button>
     </nav>
   )
 }
